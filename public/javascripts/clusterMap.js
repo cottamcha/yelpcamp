95e0694aafde8cc5eeb4b612d7b054f005e5a4b8
Jacob Cottam <jacobcottam@gmail.com>
2020-11-01 16:50:39 +0000

    4th deploy

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/dark-v10',
-    center: [-103.59179687498357, 40.66995747013945],
-    zoom: 3
+    center: [ -2.408727,54.399594 ],
+    zoom: 4
 });
 
 map.addControl(new mapboxgl.NavigationControl());
